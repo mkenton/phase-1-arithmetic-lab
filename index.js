9e1dba8551675b40032504313ef038400d1b3679
Michael <mkenton2020@icloud.com>
2021-07-19 00:12:11 -0500

    commit tree f8fff242d744c1740f419dced117ded89ca05896

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
+const num1=31, num2=2
+let multiply = num1*num2;
 
-// Write your code here
+let random = Math.floor((Math.random()*100+1));
+
+let num3=32, num4=7;
+let mod = num3 % num4;
+
+let max = Math.max(20,10,0,-10,-20,-30);
